ccc71dbde88251218e3ed1ca8ceda979de4bc337
QXU1114 <QXU1114@LW09736441.africa.bmw.corp>
2018-03-05 10:51:30 +0200

    Merge origin/master
    Conflicts:
    	README.md

---
+++ spring-boot-jms/HornetqSpringBoot/src/main/java/com/bmw/HornetqApplication.java
@@ -0,0 +1,12 @@
+package com.bmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HornetqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HornetqApplication.class, args);
+	}
+}
---
+++ spring-boot-jms/HornetqSpringBoot/src/main/java/com/bmw/HornetqReceiver.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bmw;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Lukisi Motsoeneng
+ */
+@Component
+public class HornetqReceiver
+{
+    @JmsListener(destination = "OrdersQueue")
+    public void receiveMessage()
+    {
+        
+    }
+}
